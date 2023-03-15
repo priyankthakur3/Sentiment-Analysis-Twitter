2c83c9b8f72ecfaf66b0b21a989ef0816f6db99b
Priyank Thakur <priyankthakur3@live.com>
2023-03-14 22:34:10 -0400

    Added Tweets route

--- server_js/routes/index.js
+++ server_js/routes/index.js
@@ -1,6 +1,8 @@
 import locationRoute from "../routes/locations.js";
+import tweetRoute from "../routes/tweets.js";
 const constructorMethod = (app) => {
   app.use("/locations", locationRoute);
+  app.use("/tweets", tweetRoute);
   app.use("*", (req, res) => {
     res.status(404).json({ error: "Bummer!!! Page Not Found" });
   });
