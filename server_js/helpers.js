e9711e004c5e7106b9263375de293d70d94adb44
Priyank Thakur <priyankthakur3@live.com>
2023-03-14 22:33:38 -0400

    updated helpers

--- server_js/helpers.js
+++ server_js/helpers.js
@@ -16,4 +16,34 @@ const isID = (id) => {
   return id;
 };
 
-export { isID };
+const isString = (varName, varVal) => {
+  /**
+   * Function to check if string is string or not
+   * Input Variable Name, Variable Value in String
+   * Return type: trim varVal in String
+   */
+  if (typeof varName !== "string" || varName.trim().length < 1)
+    throw new Error(`Expected VarName to be non-empty String`);
+
+  if (typeof varVal !== "string" || varVal.trim().length < 1)
+    throw new Error(`Expected ${varName} to be non-empty String`);
+
+  return varVal.trim();
+};
+
+const isNumber = (varName, varVal) => {
+  /**
+   * Function to check if varVal is Number or not
+   * Input Variable Name, Variable Value in String
+   * Return type: trim varVal in number
+   */
+  if (typeof varName !== "string" || varName.trim().length < 1)
+    throw new Error(`Expected VarName to be non-empty String`);
+
+  if (typeof varVal !== "number" || isNaN(varVal))
+    throw new Error(`Expected ${varName} to be Number`);
+
+  return varVal;
+};
+
+export { isID, isString, isNumber };
