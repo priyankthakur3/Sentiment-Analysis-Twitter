5dd06eed35354a6d125440a751e0d249d88b1105
Priyank Thakur <priyankthakur3@live.com>
2023-03-14 22:33:58 -0400

    new functions

--- server_js/data/tweets.js
+++ server_js/data/tweets.js
@@ -1,6 +1,6 @@
 import { ObjectId } from "mongodb";
 import { tweets } from "../config/mongoCollection.js";
-import { isID } from "../helpers.js";
+import { isID, isNumber, isString } from "../helpers.js";
 
 const getAll = async () => {
   const tweetsCollection = await tweets();
@@ -12,6 +12,55 @@ const getAll = async () => {
     return tweet;
   });
 
+  return tweetsList;
+};
+
+const getStats = async () => {
+  let tweetsCollection = await tweets();
+  let tweetStats = await tweetsCollection.find({});
+};
+
+const getNRecords = async (startindex, maxRecords = 50) => {
+  try {
+    startindex = isNumber("Start Index", startindex);
+  } catch (error) {
+    throw error;
+  }
+  try {
+    maxRecords = isNumber("Records", maxRecords);
+  } catch (error) {
+    throw error;
+  }
+  const tweetsCollection = await tweets();
+  let tweetsList = await tweetsCollection
+    .find({})
+    .skip(startindex)
+    .limit(maxRecords)
+    .toArray();
+  tweetsList = tweetsList.map((tweet) => {
+    tweet._id = tweet._id.toString();
+    tweet.tweet_id = tweet.tweet_id.toString;
+
+    return tweet;
+  });
+
+  return tweetsList;
+};
+
+const getTweetWOEID = async (woeid) => {
+  try {
+    woeid = isNumber("WOEID", woeid);
+  } catch (e) {
+    throw e;
+  }
+  const tweetsCollection = await tweets();
+  let tweetsList = await tweetsCollection.find({ WOEID: woeid }).toArray();
+  tweetsList = tweetsList.map((tweet) => {
+    tweet._id = tweet._id.toString();
+    tweet.tweet_id = tweet.tweet_id.toString;
+
+    return tweet;
+  });
   return tweetsList;
 };
 
@@ -34,4 +83,4 @@ const get = async (id) => {
   return tweetObj;
 };
 
-export default { getAll, get };
+export default { getAll, get, getTweetWOEID, getNRecords };
